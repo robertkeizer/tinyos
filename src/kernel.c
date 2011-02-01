10be543c06d4b4895ccce8f6008f794b1efc35ed
Robert Keizer <robert@keizer.ca>
2011-02-01 14:01:39 -0600

    Basic inclusion of mutliboot.

--- src/kernel.c
+++ src/kernel.c
@@ -1,11 +1,14 @@
+#include "multiboot.h"
 #include "kprint.h"
-void kmain( void *mbd, unsigned int magic ){
+
+void kmain( void* mbd, unsigned int magic ){
+
 	if ( magic != 0x2BADB002 ){
 		kprint( "Ack. No multiboot." );
 		for( ;; );
-	}
+	};
 
-	char *boot_loader_name = (char *) ((long*)mbd)[16];
+	char * boot_loader_name =(char*) ((long*)mbd)[16];
 
 	kprint( boot_loader_name );
 
